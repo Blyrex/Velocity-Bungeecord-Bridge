6434ed289136052169f78f58088ad618bc606024
0utplay <aldin@sijamhodzic.de>
2021-03-30 13:43:47 +0200

    Implement the ListenerInfo in the pendingconnection

--- src/main/java/com/github/velocity/bridge/connection/BridgePendingConnection.java
+++ src/main/java/com/github/velocity/bridge/connection/BridgePendingConnection.java
@@ -1,8 +1,11 @@
 package com.github.velocity.bridge.connection;
 
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.config.ProxyConfig;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ListenerInfo;
@@ -10,12 +13,16 @@ import net.md_5.bungee.api.connection.PendingConnection;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 public class BridgePendingConnection implements PendingConnection {
 
     private final Player player;
+    private final ProxyServer proxyServer;
 
     @Override
     public String getName() {
@@ -34,7 +41,29 @@ public class BridgePendingConnection implements PendingConnection {
 
     @Override
     public ListenerInfo getListener() {
-        return null;
+        ProxyConfig proxyConfig = this.proxyServer.getConfiguration();
+
+        Map<String, String> forcedHosts = new HashMap<>();
+
+        proxyConfig.getForcedHosts().forEach((key, value) -> {
+            forcedHosts.put(key, value.get(0));
+        });
+
+        return new ListenerInfo(
+                this.getSocketAddress(),
+                LegacyComponentSerializer.legacySection().serialize(proxyConfig.getMotd()),
+                proxyConfig.getShowMaxPlayers(),
+                proxyConfig.getShowMaxPlayers(),
+                new ArrayList<>(proxyConfig.getServers().values()),
+                false,
+                forcedHosts,
+                "GLOBAL_PING",
+                true,
+                false,
+                25577,
+                false,
+                false
+        );
     }
 
     @Override
