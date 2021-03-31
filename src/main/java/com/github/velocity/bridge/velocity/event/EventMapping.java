787494ad55532eb6d699a879191cdaa2a774325c
Blyrex <blyrexx@gmail.com>
2021-03-31 14:52:03 +0200

    code cleanup

--- src/main/java/com/github/velocity/bridge/velocity/event/EventMapping.java
+++ src/main/java/com/github/velocity/bridge/velocity/event/EventMapping.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import net.md_5.bungee.api.plugin.Event;
 
 @Getter
-public abstract class EventMapping<S, T> {
+public abstract class EventMapping<S, T extends Event> {
     protected final BungeeVelocityBridgePlugin plugin;
     protected final Class<S> sourceEventClass;
     protected final Class<T> targetEventClass;
@@ -20,7 +20,7 @@ public abstract class EventMapping<S, T> {
         this.plugin.getServer().getEventManager().register(this.plugin, this.sourceEventClass, this.postOrder, event -> {
             T target = this.prepare(event);
             this.plugin.getBungeeProxyServer().getPluginManager()
-                    .callEvent((Event) target);
+                    .callEvent(target);
             this.post(event, target);
         });
     }
