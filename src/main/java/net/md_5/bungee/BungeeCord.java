45472a774d2c6a5ae1e70be85205bc047513bbb5
Blyrex <blyrexx@gmail.com>
2021-07-23 20:41:50 +0200

    Fix BungeeCord.getInstance

--- src/main/java/net/md_5/bungee/BungeeCord.java
+++ src/main/java/net/md_5/bungee/BungeeCord.java
@@ -4,8 +4,11 @@ import com.github.velocity.bridge.BridgeProxyServer;
 import com.github.velocity.bridge.BungeeVelocityBridgePlugin;
 
 public class BungeeCord extends BridgeProxyServer {
-
     public BungeeCord(BungeeVelocityBridgePlugin bungeeVelocityBridgePlugin) {
         super(bungeeVelocityBridgePlugin);
+    }
+
+    public static BungeeCord getInstance() {
+        return (BungeeCord) BridgeProxyServer.getInstance();
     }
 }
